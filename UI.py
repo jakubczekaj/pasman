9c68efc6cbec03f19bded7bf9affc9366e21970e
Jakub Czekaj <j.czekaj.agh@gmail.com>
2018-11-16 13:38:36 +0100

    Overview description

---
+++ UI.py
@@ -0,0 +1,19 @@
+# Version 0.1
+
+# Usage:
+# pasman.py <keyfile>
+#   1. if file exists, prompt for a main password to decrypt it
+#   2. if file does not exist, create one and prompt for a new main password to encrypt it
+#   After that a new shell opens with below options:
+#       a) list all/subset (e.g. by typing a 'site' value)
+#       b) delete item (by list number?)
+#       c) add new (provide a way to propose randomized password to the user)
+#       d) edit
+#       e) renumber the list (optional)
+#       f) change main password for file encryption (optional)
+#
+# pasman.py # or --help, or -h
+#
+#
+
+# TODO: how to manage access hierarchy (e.g. different passwords for work, different personal passwords, etc...)?
