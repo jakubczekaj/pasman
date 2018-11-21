07fb39080e5e52c8a9cd75d5f4e317fda6ab1ad5
Jakub Czekaj <j.czekaj.agh@gmail.com>
2018-11-21 12:55:09 +0100

    keyfile read/write methods

--- Keyfile.py
+++ Keyfile.py
@@ -1,7 +1,21 @@
 # Version: 0.1
 
-# Class for managing keyfile alone (creating json, encryption/decryption of each line (?))
-
-
-# json format (after decryption):
+# Class for managing keyfile (creating pass file, encryption/decryption of each line)
+#
+# format (after decryption):
 #       site username password
+
+class Keyfile():
+    def __init__(self, path):
+        self.path = path
+
+    def write_line(self, path, line):
+        with open(path, 'a') as f:
+            f.write(line)
+
+    def read_line(self, path, num):
+        with open(path, 'r') as f:
+            for i, line in enumerate(f):
+                if i == num:
+                    return line
+
