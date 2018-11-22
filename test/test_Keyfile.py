e305c8b24bc918ad1bee2e6db7a3d5f32ad9bb27
Jakub Czekaj <j.czekaj.agh@gmail.com>
2018-11-22 08:55:41 +0100

    TestKeyfile has to inherit from unittest.TestCase

--- test/test_Keyfile.py
+++ test/test_Keyfile.py
@@ -1,8 +1,11 @@
 import unittest
 import Keyfile
 
-class TestKeyfile():
+class TestKeyfile(unittest.TestCase):
     def test_write_line(self):
         k = Keyfile.Keyfile("some/path")
         k.write_line("blablabla-line1")
         #TODO How to unit test that? Which assert to test?? Shouldn't write_line return something?
+
+if __name__ == '__main__':
+    unittest.main()
